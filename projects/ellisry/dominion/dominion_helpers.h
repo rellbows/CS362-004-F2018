931284ec2dcb456d14be7d7a6456cb1af1d24fec
rellbows <ryan.ellis927@gmail.com>
2018-10-13 15:56:18 -0500

    Refactored adventurer and smithy sections of cardEffects function.

--- projects/ellisry/dominion/dominion_helpers.h
+++ projects/ellisry/dominion/dominion_helpers.h
@@ -11,5 +11,7 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int adventurerCardEffect(struct gameState *state);
+int smithyCardEffect(int handPos, struct gameState *state);
 
 #endif
